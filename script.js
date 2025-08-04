31d590a0c33258e92969b5195c66f004b5d07bce
sheng-smo <lims2@fvhs.school.nz>
2025-08-04 12:37:13 +1200

    parallax rework

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var rea = 7
-console.log(rea);
+// console.log(rea);
 
 window.onload = function start() {
     loop();
 }
 
+var change = [1,1,1]
+
 function loop() {
     window.setInterval(function() {
-        document.getElementById("title").style.transform = "translate3d(-50%, 20vw, -10px) rotate3d(0, 1, 0, "+rea+"deg)";
+        document.getElementById("title").style.transform = "translate3d(-50%, 20vw, 0px) scale(10) rotate3d(1, 1, 1, "+rea+"deg) scale(0.1)";
         // document.getElementById("ticker").innerHTML = rea;
         rea++;
+        // const thing = document.getElementsByClassName("parallax")
+        // console.log(change)
     }, 0)
 }
