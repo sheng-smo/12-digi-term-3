102ed7515ecfb561452d04babb59108e6afe8dd8
sheng-smo <lims2@fvhs.school.nz>
2025-08-06 14:55:51 +1200

    6/8

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ window.onload = function start() {
     loop();
 }
 
-var change = [1,1,1]
 
 function loop() {
     window.setInterval(function() {
-        document.getElementById("title").style.transform = "translate3d(-50%, 20vw, 0px) scale(10) rotate3d(1, 1, 1, "+rea+"deg) scale(0.1)";
+        var scroll = document.getElementById("parallax").scrollTop / (document.body.scrollHeight/100);
+        document.getElementById("title").style.transform = "translate3d(-50%, 20vw, 0px) scale(10) rotate3d(1, 0, 0, "+-(scroll)+"deg) scale(0.1)";
         // document.getElementById("ticker").innerHTML = rea;
         rea++;
+        // console.log(document.body.scrollHeight);
         // const thing = document.getElementsByClassName("parallax")
         // console.log(change)
-    }, 0)
+    }, 1)
 }
