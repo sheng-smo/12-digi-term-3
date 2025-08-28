2b9c7d73bc59cc73fd529d27437f1647f5ab81bd
sheng-smo <lims2@fvhs.school.nz>
2025-08-29 10:24:54 +1200

    29/08

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-var rea = 7
+// var rea = 7
 // console.log(rea);
 
-window.onload = function start() {
-    loop();
-}
+// window.onload = function start() {
+//     loop();
+// }
 
 
-function loop() {
-    window.setInterval(function() {
-        // var scroll = document.getElementById("parallax").scrollTop / (document.body.scrollHeight/100);
-        // document.getElementById("title").style.transform = "translate3d(-50%, 20vw, 0px) scale(10) rotate3d(1, 0, 0, "+-(scroll)+"deg) scale(0.1)";
-        // document.getElementById("ticker").innerHTML = rea;
-        rea++;
-        // console.log(document.body.scrollHeight);
-        // const thing = document.getElementsByClassName("parallax")
-        // console.log(change)
-    }, 1)
+// function loop() {
+//     window.setInterval(function() {
+//         // var scroll = document.getElementById("parallax").scrollTop / (document.body.scrollHeight/100);
+//         // document.getElementById("title").style.transform = "translate3d(-50%, 20vw, 0px) scale(10) rotate3d(1, 0, 0, "+-(scroll)+"deg) scale(0.1)";
+//         // document.getElementById("ticker").innerHTML = rea;
+//         rea++;
+//         // console.log(document.body.scrollHeight);
+//         // const thing = document.getElementsByClassName("parallax")
+//         // console.log(change)
+//     }, 1)
+// }
+
+function confirm() {
+    const value = document.getElementById("inputawesome").value
+    document.getElementById("labelawesome").textContent = "hi"
 }
