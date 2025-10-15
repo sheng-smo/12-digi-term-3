5132bcaeada0b328e9a978a30e5c61d1b91471d4
sheng-smo <lims2@fvhs.school.nz>
2025-10-15 14:48:33 +1300

    what the forms

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ var rea = 7
 const markiplier = "mark"
 const thebiteof87 = 1987
 
-window.onload = function start() {
-    loop();
-}
+// window.onload = function start() {
+//     loop();
+// }
 
 
-function loop() {
-    window.setInterval(function() {
-        if (rea%thebiteof87 == 0) {
-            console.log("hi")
-        }
-        rea++;
-    }, 1)
-}
+// function loop() {
+//     window.setInterval(function() {
+//         if (rea%thebiteof87 == 0) {
+//             console.log("hi")
+//         }
+//         rea++;
+//     }, 1)
+// }
 
 
 function confirm(event) {
@@ -24,3 +24,20 @@ function confirm(event) {
     }
 }
 
+var value1 = document.getElementById("maps").value
+function changeMap(){
+    value1 = document.getElementById("maps").value;
+    console.log(value1);;
+    if(value1=="4"){
+        document.getElementById("map").src="asset/bases/season 4/villager.png";
+    }
+    if(value1=="5"){
+        document.getElementById("map").src="asset/bases/season 5/banner.png";
+    }
+    if(value1=="6"){
+        document.getElementById("map").src="asset/bases/season 6/10-09-2025,utah.png";
+    }
+    if(value1=="7"){
+        document.getElementById("map").src="asset/bases/season 7/isladehatsunemiku.png";
+    }
+}
